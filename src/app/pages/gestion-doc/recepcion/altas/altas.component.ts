99c18737cdd76308b2680c5b7f7b9c4f89bdae7c
PEREZ MATIAS <id17110@dasc.edu.mx>
2023-12-20 11:47:20 -0600

    Creación de estructura de carpetas y componentes

---
+++ src/app/pages/gestion-doc/recepcion/altas/altas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-altas',
+  standalone: true,
+  imports: [],
+  templateUrl: './altas.component.html',
+  styleUrl: './altas.component.css'
+})
+export class AltasComponent {
+
+}
---
+++ src/app/pages/instrumentos/instrumentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instrumentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './instrumentos.component.html',
+  styleUrl: './instrumentos.component.css'
+})
+export class InstrumentosComponent {
+
+}
